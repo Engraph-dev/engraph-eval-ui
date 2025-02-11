76409ea69091285d68582e8ecebb20b7eafcd963
Arnav Deo <arnav.nitin.deo@gmail.com>
2025-02-12 02:04:25 +0530

    fix: 🐛 Set retry: false in queryConfig to prevent request spam

--- src/components/wrappers/query-provider.tsx
+++ src/components/wrappers/query-provider.tsx
@@ -14,6 +14,7 @@ export default function QueryProviderWrapper({
 				refetchOnMount: false,
 				refetchOnWindowFocus: false,
 				staleTime: 1000 * 60 * 5, // 5 minutes
+				retry: false,
 			},
 		},
 	})
